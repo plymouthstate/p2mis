11a862993825d3633a877bf4a2ebb6cd072a0176
Adam Backstrom <adam@sixohthree.com>
2012-01-12 13:16:54 -0500

    Implement individual protips using CSS

--- p2mis.js
+++ p2mis.js
@@ -60,51 +60,6 @@ var a = $('<a href="#">Enable desktop notifications</a>.').bind('click', functio
 });
 $('#footer p').append(a);
 
-$('ul#postlist .tag-protip').prepend('<h2 class="protip"><span class="accent">&#10144;</span> Ultra Awesome <span class="what"></span> Pro Tip:</h2>');
-$('ul#postlist .tag-protip.tag-vim')
-	.find('.what')
-	.html('PHP')
-	.end()
-	.attr('title', 'PHP Pro Tip');
-
-$('ul#postlist .tag-protip.tag-vim, ul#postlist .tag-protip.tag-unix, ul#postlist .tag-protip.tag-linux')
-	.find('.what')
-	.html('Command Line')
-	.end()
-	.find('.accent')
-	.attr('title', 'Command Line Pro Tip')
-	.html('&#9000;'); // keyboard
-
-$('ul#postlist .tag-protip.tag-database, ul#postlist .tag-protip.tag-oracle, ul#postlist .tag-protip.tag-mysql')
-	.find('.what')
-	.html('Database')
-	.end()
-	.find('.accent')
-	.attr('title', 'Database Pro Tip')
-	.html('&#9638;'); // hash box
-
-$('ul#postlist .tag-protip.tag-vim')
-	.find('.what')
-	.html('VIM')
-	.end()
-	.find('.accent')
-	.attr('title', 'VIM Pro Tip')
-	.html('&#9998;'); // pencil
-
-$('ul#postlist .tag-protip.tag-git')
-	.find('.what')
-	.html('Git')
-	.end()
-	.find('.accent')
-	.attr('title', 'Git Pro Tip')
-	.html('&#10026;'); // circled star
-
-$('ul#postlist .tag-protip.tag-warning')
-	.find('.what')
-	.append(' Warning')
-	.end()
-	.find('.accent')
-	.attr('title', 'Pro Warning')
-	.html('&#9760;'); // skull and crossbones
+$('ul#postlist .tag-protip').prepend('<h2 class="protip"><span class="accent"></span> Ultra Awesome <span class="what"></span> Pro Tip:</h2>');
 
 })(jQuery);
